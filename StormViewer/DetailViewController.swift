fb38df84d1b13f4ce3274fa6db34d5fb45c64d9d
Regina Mayorova <gina.rebrown@gmail.com>
2023-02-10 06:37:38 +0500

    commit

---
+++ StormViewer/DetailViewController.swift
@@ -0,0 +1,50 @@
+//
+//  DetailViewController.swift
+//  StormViewer
+//
+//  Created by Regina Mayorova on 04.02.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    var selectedPictureNumber = 0
+    var totalPictures = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Picture \(selectedPictureNumber) of \(totalPictures)"
+        navigationItem.largeTitleDisplayMode = .never
+
+        if let imageToLoad = selectedImage {
+            imageView.image  = UIImage(named: imageToLoad)
+        }
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+    class ViewController: UITableViewController {
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
 
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is a picture to load!
+                pictures.append(item)
+            }
+        }
+        pictures.sort()
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+        
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+        //Asks the data source for a cell to insert in a particular location of the table view.
+        {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+            cell.textLabel?.text = pictures[indexPath.row]
+            return cell
+        }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            // 2: success! Set its selectedImage property
+            vc.selectedImage = pictures[indexPath.row]
+            vc.selectedPictureNumber = indexPath.row + 1
+            vc.totalPictures = pictures.count
+
+            // 3: now push it onto the navigation controller
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    
 
 }
 
